db13996cf88492f6748b89d57a3c491c9e95c2fd
pkoprov <koprovp@gmail.com>
2021-05-31 14:13:27 -0400

    working version

--- Publisher.py
+++ Publisher.py
@@ -2,17 +2,18 @@ import telnetlib, json, time
 from datetime import datetime
 import paho.mqtt.client as mqtt
 
-with open("Haas-Data-Collection/Pub_config.txt") as config:
+with open("/home/pi/Haas-Data-Collection/Pub_config.txt") as config:
     mqttBroker = config.readline()
     client = config.readline()
 
 mqttBroker = mqttBroker.replace("\n","")
 port = 1883
+msg = client
 
 client = mqtt.Client(client)
 client.connect(mqttBroker, port)
-msg = ("This is {}".format(client))
+
 while True:
     client.publish("HaasData", msg, qos=0)
-    print("Just published", datetime.now().strftime("%H:%M:%S"))
+    print("Just published", msg, datetime.now().strftime("%H:%M:%S"))
     time.sleep(1)
