5a159130b68c9bbad6a873985ea53157c6858ae0
Pavel "Pasha" Koprov <koprovp@gmail.com>
2021-06-08 19:48:43 -0400

    minor bug fixes

--- Publisher.py
+++ Publisher.py
@@ -2,8 +2,8 @@ import telnetlib, json, time
 import paho.mqtt.client as mqtt
 import pandas as pd
 
-Q_codes = pd.read_excel("./Haas-Data-Collection/Q-codes.xlsx", sheet_name="Global")
-Q_codes = Q_codes.append(pd.read_excel("./Haas-Data-Collection/Q-codes.xlsx",sheet_name="Macros"), ignore_index = True)
+Q_codes = pd.read_excel("./Haas-Data-Collection/Global Q-codes.xlsx")
+Q_codes = Q_codes.append(pd.read_excel("./Haas-Data-Collection/Q-codes.xlsx"), ignore_index = True)
 
 
 # read data specific to setup and machines
@@ -25,22 +25,32 @@ tn = telnetlib.Telnet(CNC_host, CNC_port)
 
 
 while True:
+    # dictionary for the future read from telnet
     data = {}
 
+    # transform Q-codes from table to binary and send to the CNC machine
     for n, i in enumerate(Q_codes["Variable"]):
         msg = i.encode("ascii") + b"\n"
         tn.write(msg)
 
+    # read data from CNC and parse it
     out = tn.read_until(msg, 0.5).decode("utf-8").replace(">", '').replace("\r\n", "|").split("|")
     out.pop(-1)
 
-    for n, value in enumerate(out):
-        val_list = value.split(", ")
-        if value[1] != "?":
-            data[Q_codes["Description"][n]] = val_list[1]
+    # fill the dictionary with data
+    for n, msg in enumerate(out):
+        val_list = msg.split(", ")  # split message into topic and value
+        if n == 12:
+            data["Status"] = msg
+            # print(msg)
+        elif val_list[1] != "?": # if value exists
+            var = Q_codes["Description"][n]
+            data[var] = val_list[1]
+            # print(var, val_list[1])
 
     jsondata = json.dumps(data)
     client.publish(topic, jsondata, qos=0)
 
-    print("Data published in topic {}".format(topic))
+    print(f"Data published in topic {topic} {data}")
     time.sleep(1)
+
--- Q-codes.py
+++ Q-codes.py
@@ -16,14 +16,11 @@ for n, i in enumerate(all_macros["NGC Variable"]):
         for m, j in enumerate(range(i1,i2+1)):
             code = pd.DataFrame([[f"?Q600 {j}", f"{all_macros['Usage'][n]} {m+1} (Var {j})"]], columns=columns)
             codes = codes.append(code, ignore_index=True)
-            # print(all_macros["Usage"][n], m+1)
-            # print(j)
 
     else:
         i = int(i[0])
         code = pd.DataFrame([[f"?Q600 {i}", all_macros["Usage"][n]]], columns=columns)
         codes = codes.append(code, ignore_index=True)
-        # print(i)
     print(codes.tail())
 
-codes.to_excel("Q-codes.xlsx", index=False)
+codes.to_excel("Q-codes.xlsx", index=False, sheet_name= "Macros")
