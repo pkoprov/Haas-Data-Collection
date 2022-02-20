0bf7a652d6042e6b4d54a1c53dcb7af9fe00b0eb
pkoprov <koprovp@gmail.com>
2022-02-19 20:28:45 -0500

    changes the names of variables and functions

--- Publisher.py
+++ Publisher.py
@@ -26,31 +26,35 @@ def parse(telnetdata):
     return data_dict
 
 
-def publish(telnetdata):
-    data = parse(telnetdata)
-    jsondata = json.dumps(data)
-    client.publish(f"data/{topic}", jsondata, qos=0)
-    print(f"Data published in topic {topic} {data}")
+def publish_device_data(telnetdata):
+    raw_data = parse(telnetdata)
+    jsondata = json.dumps(raw_data)
+
+    data_topic = f"spBv1.0/FWH2200/DDATA/{asset}_RPI/{asset}"
+
+    client.publish(data_topic, jsondata, qos=2)
+    print(f"Data published in topic {data_topic} {raw_data}")
 
 
 def telnet_connection(fail_message):
-    global tn, telnetstat, tn_err_msg
+    global tn, telnet_status, tn_err_msg
     try:
         client.publish(f"ping/{topic}", '')
         tn = telnetlib.Telnet(CNC_host, CNC_port, 1)
-        telnetstat = True
+        telnet_status = True
         tn_err_msg = False
         client.publish(f"ping/{topic}", "Telnet connected")
+
         client.publish(f"spBv1.0/FWH2200/DBIRTH/{asset}_RPI/{asset}", 'ONLINE', retain=True)
 
     except:
+        telnet_status = False
         if not tn_err_msg:
             client.publish(f"error/{topic}", fail_message)
             print(fail_message)
             tn_err_msg = True
         else:
             client.publish(f"ping/{topic}", '')
-        telnetstat = False
 
 
 with open("/home/pi/Haas-Data-Collection/DB Table columns.csv") as text:
@@ -68,36 +72,32 @@ for i,j in enumerate(Q_codes):
 with open("/home/pi/Haas-Data-Collection/Pub_config.txt") as config:
     # "/home/pi/Haas-Data-Collection/Pub_config.txt"
     mqttBroker = config.readline().split(" = ")[1].replace("\n", "")
-    client = config.readline().split(" = ")[1].replace("\n", "")
+    asset = config.readline().split(" = ")[1].replace("\n", "")
     CNC_host = config.readline().split(" = ")[1].replace("\n", "")
 
-
-topic = client
+topic = asset
 # topic = "HaasData"
 CNC_port = 5051
 MQTT_port = 1883
-telnetstat = False
+telnet_status = False
 tn_err_msg = False
 
-
-client = mqtt.Client(client)
+client = mqtt.Client(asset)
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 client.will_set(f"spBv1.0/FWH2200/NDEATH/{asset}_RPI", 'OFFLINE', retain=True)
 client.connect(mqttBroker, MQTT_port, keepalive=10)
 client.loop_start()
 
-while not telnetstat:
+while not telnet_status:
     telnet_connection("Initial Telnet connection failed!")
-
 
 last_out = []
 omit = [Q_codes.index('?Q600 3012'),Q_codes.index('?Q300'), Q_codes.index('?Q600 3020')]
 
-
 while True:
 
-    if telnetstat:
+    if telnet_status:
         # transform Q-codes from table to binary and send to the CNC machine
         try:
             for i in Q_codes:
@@ -105,7 +105,7 @@ while True:
                 tn.write(msg)
         except:
             client.publish(f"error/{topic}", "Telnet connection failed!: problem writing")
-            telnetstat = False
+            telnet_status = False
             tn_err_msg = True
 
         try:
@@ -114,7 +114,7 @@ while True:
             out[48] = "MACRO, "+ str(round(float(out[48].split(", ")[1]))) # round RPM to integer
         except:
             client.publish(f"error/{topic}", "Telnet connection failed!: problem reading")
-            telnetstat = False
+            telnet_status = False
             tn_err_msg = True
 
         try:
@@ -127,19 +127,19 @@ while True:
                     pass
                 elif new_out:
                     print("new data")
-                    publish(out)
+                    publish_device_data(out)
                     last_out = new_out
                     time.sleep(1)
             else:
                 print('initial message')
                 if out:
-                    publish(out)
+                    publish_device_data(out)
                     last_out = out[:omit[0]] + out[omit[0] + 1:omit[1]] + out[omit[1] + 1:omit[2]] + out[omit[2] + 1:]
                 time.sleep(1)
 
         except:
             client.publish(f"error/{topic}", "Parse and publish failed!")
-            telnetstat = False
+            telnet_status = False
             tn_err_msg = True
     else:
         telnet_connection("Telnet connection failed!")
