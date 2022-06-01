582c9e9c98feccd8e708b194e3260ff85e88b2e1
Pavel "Pasha" Koprov <84463424+pkoprov@users.noreply.github.com>
2022-06-01 16:45:07 -0400

    Adds more context and flexibility for the input

--- Create Table app.py
+++ Create Table app.py
@@ -2,12 +2,17 @@ import psycopg2 as pg
 import pandas as pd
 
 
-db = "HaasDataCollection"
-conn = pg.connect(f"dbname={db} user=postgres password='fwh2200'")
+with open(r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\historian.config", 'r') as config:
+    mqttBroker = config.readline().split(" = ")[1].replace("\n", "")
+    myGroupId = config.readline().split(" = ")[1].replace("\n", "")
+    dbName = config.readline().split(" = ")[1].replace("\n", "")
+    myUsername = config.readline().split(" = ")[1].replace("\n", "")
+    myPassword = config.readline().split(" = ")[1].replace("\n", "")
 
+conn = pg.connect(f"dbname={dbName} user={myUsername} password={myPassword}")
 
 def createTable():
-    insertCMD = f'CREATE TABLE IF NOT EXISTS public."{topic}" ({columns});'
+    insertCMD = f'CREATE TABLE IF NOT EXISTS "{schema}"."{topic}" ({columns});'
     cur.execute(insertCMD)
 
 try:
@@ -16,39 +21,50 @@ try:
 except (Exception, pg.DatabaseError) as error:
     print(error)
 
+schemas = cur.execute("select schema_name from information_schema.schemata;")
+schemas = cur.fetchall()
+
+schema = input(f"Enter schema ({'/'.join([i[0] for i in schemas])}): ")
+path = r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\DB Table columns.xlsx"
+file = pd.ExcelFile(path)
+
 running = True
 while running:
-    topic = input("Type the table name:  ")
-    path = "DB Table columns.xlsx"
-    sheet = input("Type the sheet name: ")
+    topic = input("Type the table name: ")
+    sheet = input(f"Type the sheet name ({'/'.join(file.sheet_names)}): ")
     while True:
         try:
-            colnames = pd.read_excel(path, sheet_name=sheet)
+            colnames = file.parse(sheet_name=sheet)
             break
         except ValueError:
             print("Incorrect sheet name")
             sheet = input("Type the CORRECT sheet name: ")
     # Q_codes = Q_codes.append(pd.read_excel("Q-codes.xlsx"), ignore_index = True)
 
-    columns = ""
-    for col in colnames["Description"]:
-        columns = f'{columns}, "{col}" text'
-    columns = columns[2:]
+    columns = []
+    for i,row in colnames.iterrows():
+        dtype = "text" if row["datatype"] == "str" else "numeric"
+        columns.append(f'"{row[1]}" {dtype}, ')
+    if sheet == "Static":
+        columns.append('"IP_address" text')
+        columns = "".join(columns)
+    else:
+        columns = "".join(columns)[:-2]
+
 
     try:
         createTable()
         conn.commit()
-        print(colnames)
-        print(f"Table {topic} was created in DB {db}")
+        print(f"Table {topic} was created in DB {dbName}")
     except(Exception, pg.DatabaseError) as error:
         print(error)
 
     while True:
-        prompt = input("Create new table? ").lower()
-        if  prompt == 'yes':
+        prompt = input("Create new table?(y/n): ").lower()
+        if  prompt == 'y':
             running=True
             break
-        elif prompt == 'no':
+        elif prompt == 'n':
             running = False
             break
         else:
