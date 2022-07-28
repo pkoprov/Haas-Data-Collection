004274024a642f506a22ab03fd39538e77e73824
Pavel Koprov <pkoprov@ncsu.edu>
2022-07-27 20:53:08 -0400

    Local changes

--- MQTT_SpB/Device_client.py
+++ MQTT_SpB/Device_client.py
@@ -1,6 +1,6 @@
 import sys
 
-# sys.path.insert(0, r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\spb") # uncomment for Windows
+# sys.path.insert(0, r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\spb")  # uncomment for Windows
 sys.path.insert(0, "/home/pi/Haas-Data-Collection/spb")  # uncomment for Raspberry Pi
 
 import sparkplug_b as sparkplug
@@ -57,72 +57,82 @@ def on_message(client, userdata, msg):
 # function to get data from NGC
 def getDdata():
     payload = sparkplug.getDdataPayload()
-    n = 1
-    # send macros to NGC
-    for mac in mac_list:
-        try:
-            tn.write(mac)
-        except:
-            print(f"Telnet connection failed! Could not write {mac} to CNC machine")
-            raise ConnectionError
 
-    tn.read_until(b'>>!\r\n'*len(mac_list), timeout=1) # flush the buffer after macros
+    code = "".join([par[-1] + "\n" for par in par_list]).encode() + b"|*|\n"
+    try:
+        tn.read_very_eager()  # clear buffer
+        tn.write(code)  # send command
+    except:
+        # if connection fails, try to reconnect
+        print(f"Telnet connection failed! Could not write codes to CNC machine")
+        raise ConnectionError
+    try:
+        msg = tn.read_until(b'|*|', 1)  # read response
+    except:
+        # if connection fails, try to reconnect
+        print(f"Telnet connection failed! Could not read from CNC machine")
+        raise ConnectionError
+    if b"|*|" in msg:
+        msg = msg[:-10]
+    else:
+        print(f"Telnet connection failed! Could not read all message from CNC machine")
+        raise ConnectionError
+    try:
+        val_list = parse(msg, par_list)  # parse response
+    except ValueError:
+        print(f"{n} Empty value for {par[0]}")
+        raise ConnectionError
 
     # Add device metrics
-    for par in par_list: # iterate through the parameters
-        code = par[-1].encode() + b"\n"
-        try:
-            tn.read_very_eager() # clear buffer
-            tn.write(code) # send command
-        except:
-            # if connection fails, try to reconnect
-            print(f"Telnet connection failed! Could not write {code} ({par[0]}) to CNC machine")
-            raise ConnectionError
-        try:
-            msg = tn.read_until(b'\n', 1) # read response
-        except:
-            # if connection fails, try to reconnect
-            print(f"Telnet connection failed! Could not read from CNC machine")
-            raise ConnectionError
-        try:
-            value = parse(msg, par[0]) # parse response
-        except ValueError:
-            print(f"{n} Empty value for {par[0]}")
-            raise ConnectionError
+    for n, par in enumerate(par_list):  # iterate through the parameters
         if par[1] == 3:  # int
-            value = int(float(value))
+            val_list[n] = int(float(val_list[n]))
         elif par[1] == 9:  # float
-            value = float(value)
+            val_list[n] = float(val_list[n])
         elif par[1] == 11:  # boolean
-            value = bool(float(value))
+            val_list[n] = bool(float(val_list[n]))
         elif par[1] == 12:  # string
-            value = str(value)
-        addMetric(payload, par[0], None, par[1], value)
-        n += 1
+            val_list[n] = str(val_list[n])
+        if "Max axis load" in par[0] and val_list[n] == -1:
+            continue
+        addMetric(payload, par[0], None, par[1], val_list[n])
 
+    # send macros to NGC
+    try:
+        tn.write(mac_list)
+    except:
+        print(f"Telnet connection failed! Could not write {mac} to CNC machine")
+        raise ConnectionError
+
+    tn.read_until(b'>>!\r\n' * len(mac_list.split(b"\n")[:-1]), timeout=1)  # flush the buffer after macros
     return payload
 
 
 # function to parse output of CNC machine from telnet port
-def parse(telnetdata, par_name):
-    msg = telnetdata.decode()[:-2].replace('>', '').split(', ')
+def parse(telnetdata, par_list):
+    val_list = []
+    msg_lst = telnetdata.decode().replace(">", "").split("\r\n")
 
-    if len(msg) == 2 and msg[1] != "?":  # if value exists
-        if 'year' in par_name.lower():
-            val = time.strftime("%Y/%m/%d", time.strptime(msg[1][:-2], "%y%m%d"))
-        elif 'hour' in par_name.lower():
-            val = time.strftime("%H:%M:%S", time.strptime(msg[1][:-2], "%H%M%S"))
-        elif 'rpm' in par_name.lower():
-            val = int(float(msg[1]))
+    for msg, par in zip(msg_lst, par_list):
+        msg = msg.split(", ")
+
+        if len(msg) == 2 and msg[1] != "?":  # if value exists
+            if 'year' in par[0].lower():
+                val = time.strftime("%Y/%m/%d", time.strptime(msg[1][:-2], "%y%m%d"))
+            elif 'hour' in par[0].lower():
+                val = time.strftime("%H:%M:%S", time.strptime(msg[1][:-2], "%H%M%S"))
+            elif 'rpm' in par[0].lower():
+                val = int(float(msg[1]))
+            else:
+                val = msg[1]
+        elif ('program' and "parts") in ''.join(msg).lower():
+            val = ', '.join(msg)
+        elif 'busy' in ''.join(msg).lower():
+            val = "BUSY"
         else:
-            val = msg[1]
-    elif ('program' and "parts") in ''.join(msg).lower():
-        val = ', '.join(msg)
-    elif 'busy' in ''.join(msg).lower():
-        val = "BUSY"
-    else:
-        raise ValueError("No value found for parameter: " + par_name)
-    return val
+            raise ValueError("No value found for parameter: " + par[0])
+        val_list.append(val)
+    return val_list
 
 
 # function to publish device birth certificate
@@ -130,14 +140,11 @@ def publishDeviceBirth():
     global previous_Ddata
     print("Publishing Device Birth Certificate")
     try:
-        payload = getDdata() # get data from NGC
+        payload = getDdata()  # get data from NGC
     except:
         print("Could not get data from CNC machine")
         tn.close()
         sys.exit()
-
-    addMetric(payload, "Device Control/Rebirth", None, MetricDataType.Boolean, False)
-    addMetric(payload, "Device Control/Reboot", None, MetricDataType.Boolean, False)
 
     totalByteArray = payload.SerializeToString()
     # Publish the initial data with the Device BIRTH certificate
@@ -158,15 +165,19 @@ def publishDeviceData():
         tn.close()
         publishDeviceDeath()
         sys.exit()
-
-    for i, metric in enumerate(
-            payload.metrics):  # iterate through new metrics values to find if there was a change
+    
+    # iterate through new metrics values to find if there was a change
+    for i, metric in enumerate(payload.metrics):  
         if metric.name in ['Year, month, day', 'Hour, minute, second', 'Power-on Time (total)',
                            'Power on timer (read only)']:  # ignore these metrics
             continue
-        else:
-            previous_metric = [met for met in previous_Ddata.metrics if met.name == metric.name][
-                0]  # find previous metric matching current metric
+        else:  # find previous metric matching current metric
+            try:
+                previous_metric = [prev_met for prev_met in previous_Ddata.metrics if prev_met.name == metric.name][0]
+            except:
+                print(f"\nDevice metric has been added:\n{metric}")
+                stale = False
+                break
             if metric.name == "Coolant level" and abs(
                     metric.float_value - previous_metric.float_value) < 2:  # if coolant level is stable
                 stale = True
@@ -183,7 +194,7 @@ def publishDeviceData():
                 stale = False
                 break
     if stale:
-        pass
+        return
 
     else:
         totalByteArray = payload.SerializeToString()
@@ -191,7 +202,7 @@ def publishDeviceData():
         client.publish("spBv1.0/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, totalByteArray, 2, True)
 
         print("^Device Data has been published")
-        previous_Ddata = payload # update previous data
+        previous_Ddata = payload  # update previous data
 
 
 def publishDeviceDeath():
@@ -208,7 +219,7 @@ def publishDeviceDeath():
 
 # read data specific to setup and machines
 # with open(r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\Node.config") as config: # uncomment for Windows
-with open("/home/pi/Haas-Data-Collection/Node.config") as config: # uncomment for Raspberry Pi
+with open("/home/pi/Haas-Data-Collection/Node.config") as config:  # uncomment for Raspberry Pi
     mqttBroker = config.readline().split(" = ")[1].replace("\n", "")
     myGroupId = config.readline().split(" = ")[1].replace("\n", "")
     myNodeName = config.readline().split(" = ")[1].replace("\n", "")
@@ -235,8 +246,8 @@ client.loop_start()
 time.sleep(0.1)
 
 # read required parameters from csv file
-# with open(r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\DB Table columns.csv") as text: # uncomment for Windows
-with open("/home/pi/Haas-Data-Collection/DB Table columns.csv") as text:    # uncomment for Raspberry Pi
+# with open(r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\DB Table columns.csv") as text:  # uncomment for Windows
+with open("/home/pi/Haas-Data-Collection/DB Table columns.csv") as text:  # uncomment for Raspberry Pi
     parameters = text.read().split('\n')[:-1]
 
 # create parameter tuples
@@ -256,9 +267,9 @@ for i, par in enumerate(parameters):
     par_list.append((name, data_type, code))
 par_list = tuple(par_list)
 
-mac_list = [b"?E1064 0\n", b"?E1065 0\n", b"?E1066 0\n"]#, b"?E3196 5000.0000\n"]
+mac_list = b"?E1064 -1\n?E1065 -1\n?E3196 5000.0000\n"
 
-publishDeviceBirth() # publish birth certificate
+publishDeviceBirth()  # publish birth certificate
 
 while True:
-    publishDeviceData() # publish data if data has changed
+    publishDeviceData()  # publish data if data has changed
