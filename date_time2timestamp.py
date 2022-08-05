79c7e49861cf2e4835e7d8759fa1d8bd03cf3433
Pavel "Pasha" Koprov <84463424+pkoprov@users.noreply.github.com>
2022-08-05 14:08:06 -0400

    implement bulk update

--- date_time2timestamp.py
+++ date_time2timestamp.py
@@ -1,8 +1,21 @@
+import time
+
 import pandas as pd
 import psycopg2 as pg
+from psycopg2.extras import execute_values
+
+
+def update(con, data):
+    cur = con.cursor()
+    execute_values(cur, """UPDATE "AML"."ST-10_2" 
+                           SET "timestamp" = update_payload.timestamp 
+                           FROM (VALUES %s) AS update_payload (date,time, timestamp) 
+                           WHERE "Year, month, day" = update_payload.date AND
+                            "Hour, minute, second" = update_payload.time""", data)
+    con.commit()
+
 
 table = "ST-10_2"
-
 
 with open(r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\historian.config",
           'r') as config:  # uncomment for Windows
@@ -20,17 +33,19 @@ try:
 except (Exception, pg.DatabaseError) as error:
     print(error)
 
+dbData = pd.read_sql_query(f'SELECT * FROM "AML"."{table}" where "timestamp" is null', conn)
 
-dbData = pd.read_sql_query(f'SELECT * FROM "AML"."{table}"',   conn)
-
-date = (pd.to_datetime(dbData['Year, month, day']) + pd.Timedelta("4 hours")).values.astype(float)/10e8
-tim = pd.to_timedelta(dbData['Hour, minute, second']).values.astype(float)/10e8
+date = (pd.to_datetime(dbData['Year, month, day']) + pd.Timedelta("4 hours")).values.astype(float) / 10e8
+tim = pd.to_timedelta(dbData['Hour, minute, second']).values.astype(float) / 10e8
 dat_tim = date + tim
 
+print(dbData.shape[0], "rows to update")
 
-CMD = ''
-for date, time, stamp in zip(dbData['Year, month, day'], dbData['Hour, minute, second'], dat_tim):
-    CMD = f'''UPDATE "AML"."{table}" SET "timestamp"= {stamp} where "Year, month, day" = '{date}' and "Hour, minute, second" = '{time}'; '''
-    cur.execute(CMD)
+data = []
+for date, tim, stamp in zip(dbData['Year, month, day'], dbData['Hour, minute, second'], dat_tim):
+    data.append((date, tim, stamp))
 
-conn.commit()
+start = time.perf_counter()
+
+update(conn, data)
+print(time.perf_counter() - start)
