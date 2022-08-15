3ad92492bdde5368fb6dd82bf48efd5cc2cd3699
Pavel "Pasha" Koprov <84463424+pkoprov@users.noreply.github.com>
2022-08-15 11:29:23 -0400

    formatting

--- MQTT_SpB/Node_client.py
+++ MQTT_SpB/Node_client.py
@@ -6,7 +6,7 @@ import time
 import paho.mqtt.client as mqtt
 
 # sys.path.insert(0, r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\spb") # uncomment for Windows
-sys.path.insert(0, "/home/pi/Haas-Data-Collection/spb") # uncomment for Raspberry Pi
+sys.path.insert(0, "/home/pi/Haas-Data-Collection/spb")  # uncomment for Raspberry Pi
 
 import sparkplug_b as sparkplug
 from sparkplug_b import *
@@ -20,9 +20,9 @@ def device_ping(device_ip, timeout=1):
     ping = True
     while True:
         if not device_online:
-            if os.system("ping -c 1 " + device_ip + ' | grep "1 received"') == 0: # uncomment for Raspberry Pi
+            if os.system("ping -c 1 " + device_ip + ' | grep "1 received"') == 0:  # uncomment for Raspberry Pi
                 ping = True
-            #         if os.system("ping -c 1 " + device_ip + ' | find "Received = 1"') == 0: # uncomment for Windows
+                #         if os.system("ping -c 1 " + device_ip + ' | find "Received = 1"') == 0: # uncomment for Windows
                 print("Starting of the Device program")
                 os.system("python3 ./Device_client.py")  # uncomment for Raspberry Pi
                 # os.system("python Device_client.py") # uncomment for Windows
@@ -59,9 +59,9 @@ def on_message(client, userdata, msg):
 
     # if the message is purposed for this node
     if tokens[0] == "spBv1.0" and tokens[1] == myGroupId and tokens[3] == myNodeName:
-        inboundPayload = sparkplug_b_pb2.Payload() # create a payload object
-        inboundPayload.ParseFromString(msg.payload) # parse the payload into the payload object
-        if tokens[2] == "NCMD": # if the message is a node command
+        inboundPayload = sparkplug_b_pb2.Payload()  # create a payload object
+        inboundPayload.ParseFromString(msg.payload)  # parse the payload into the payload object
+        if tokens[2] == "NCMD":  # if the message is a node command
             for metric in inboundPayload.metrics:
                 if metric.name == "Node Control/Next Server":
                     print("'Node Control/Next Server' is not implemented in this example")
@@ -71,33 +71,35 @@ def on_message(client, userdata, msg):
                 elif metric.name == "Node Control/Reboot":
                     print("Node is going to reboot")
                     try:
-                        os.system('sudo reboot') # uncomment for Raspberry Pi
+                        os.system('sudo reboot')  # uncomment for Raspberry Pi
                     except:
                         print("Node reboot failed")
                 elif metric.name == "bdSeq":
                     pass
                 else:
                     print("Unknown command: " + metric.name)
-        elif tokens[2] == "DCMD" and tokens[4] == myDeviceName: # if the message is a device command and for this device
-            for metric in inboundPayload.metrics: # iterate through the metrics
+        elif tokens[2] == "DCMD" and tokens[
+            4] == myDeviceName:  # if the message is a device command and for this device
+            for metric in inboundPayload.metrics:  # iterate through the metrics
                 if metric.name == "Device Status":
                     if metric.string_value == "start":
                         print("Starting of the Device program")
-                        os.system("python3 ./Device_client.py") # uncomment for Raspberry Pi
+                        os.system("python3 ./Device_client.py")  # uncomment for Raspberry Pi
 
-        elif tokens[2] in ("DBIRTH", "DDEATH") and tokens[4] == myDeviceName: # if the message is a device birth or death
-#             if time.time() - inboundPayload.timestamp / 1000 < 1: # if the message is within the last second
+        elif tokens[2] in ("DBIRTH", "DDEATH") and tokens[
+            4] == myDeviceName:  # if the message is a device birth or death
             global dBirthTime, dDeathTime, device_online
             if tokens[2] == "DBIRTH":
-#                 print("Device Birth Certificate has been received")
+                # print("Device Birth Certificate has been received")
                 dBirthTime = inboundPayload.timestamp
-                
+
             elif tokens[2] == "DDEATH":
-#                 print("Device Death Certificate has been received")
+                # print("Device Death Certificate has been received")
                 dDeathTime = inboundPayload.timestamp
 
-            if all(var in globals().keys() for var in ['dBirthTime', 'dDeathTime']): # if both birth and death certificates have been received
-                if dDeathTime > dBirthTime: # if the death certificate is after the birth certificate
+            if all(var in globals().keys() for var in
+                   ['dBirthTime', 'dDeathTime']):  # if both birth and death certificates have been received
+                if dDeathTime > dBirthTime:  # if the death certificate is after the birth certificate
                     device_online = False
                 else:
                     device_online = True
@@ -180,7 +182,7 @@ def getNdata():
 
 # read data specific to setup and machines
 # with open(r"C:\Users\pkoprov\PycharmProjects\Haas-Data-Collection\Node.config") as config: # uncomment for Windows
-with open("/home/pi/Haas-Data-Collection/Node.config") as config: # uncomment for Raspberry Pi
+with open("/home/pi/Haas-Data-Collection/Node.config") as config:  # uncomment for Raspberry Pi
     mqttBroker = config.readline().split(" = ")[1].replace("\n", "")
     myGroupId = config.readline().split(" = ")[1].replace("\n", "")
     myNodeName = config.readline().split(" = ")[1].replace("\n", "")
@@ -206,16 +208,16 @@ client.connect(mqttBroker, 1883, 60)
 
 # Short delay to allow connect callback to occur
 client.loop_start()
-port = 5051 # port number for the Telnet server
+port = 5051  # port number for the Telnet server
 time.sleep(1)
 
-device_online = False # set the device online flag
+device_online = False  # set the device online flag
 # Publish Node birth certificate
 publishNodeBirth()
 
-device_NGC = threading.Thread(target=device_ping, args=(CNC_host, 1)) # thread for device pinging
+device_NGC = threading.Thread(target=device_ping, args=(CNC_host, 1))  # thread for device pinging
 device_NGC.start()
 time.sleep(3)
 
 while True:
-    publishNdata() # publish NDATA if it changes
+    publishNdata()  # publish NDATA if it changes
